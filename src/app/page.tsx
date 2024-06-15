3c975347ba47f830b8e10b89f46ae5031695bb17
Ibrahim-el-moudden <79118459+Ibrahim-el-moudden@users.noreply.github.com>
2024-06-15 19:02:43 +0200

    Layout fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,22 @@ import {SkillsSection} from "@/containers/skills";
 import {ProjectsSection} from "@/containers/projects";
 import {ContactSection} from "@/containers/contact";
 import {ScrollToTop} from "@/components/scroll-to-top";
+import {Navbar} from "@/components/navigation/navbar";
+import {Footer} from "@/components/navigation/footer";
 
 export default function Home() {
   return (
-      <>
+      <main className="pt-12">
+          <Navbar/>
           <div className="container space-y-16 py-28">
               <HomeSection/>
               <AboutSection/>
               <SkillsSection/>
               <ProjectsSection/>
               <ContactSection/>
-              <ScrollToTop/>
           </div>
-      </>
+          <ScrollToTop/>
+          <Footer/>
+      </main>
   );
 }
--- src/components/navigation/logo.tsx
+++ src/components/navigation/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Logo() {
     return (
         <a href="/">
-            <Image src="/logo.svg" alt="logo" width={160} height={40} />
+            <Image src="/logo.png" alt="logo" width={160} height={40} />
         </a>
     );
 
