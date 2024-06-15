09afb53a1687290e839841451f93e8674adffd52
Ibrahim-el-moudden <79118459+Ibrahim-el-moudden@users.noreply.github.com>
2024-06-15 16:38:15 +0200

    Contact section

--- src/containers/contact.tsx
+++ src/containers/contact.tsx
@@ -1,6 +1,19 @@
+import {SectionTitle} from "@/components/section-title";
+import { buttonVariants } from "@/components/ui/button";
+import { Mail } from "lucide-react";
+import React from "react";
 
 export function ContactSection() {
-    return(
-        <div>Contact section</div>
+    return (
+        <section
+            id="contact"
+            className="relative bg-card bg-opacity-80 backdrop-blur-md border rounded-xl p-6 flex flex-col items-center gap-y-6 md:gap-y-12"
+        >
+            <SectionTitle title="Get in touch" description="Let's work together" />
+            <a href="mailto:elmouddenibrahim@gmail.com" className={buttonVariants({ size: "lg" })}>
+                <Mail className="size-4" />
+                <span>Si hello</span>
+            </a>
+        </section>
     )
 }
